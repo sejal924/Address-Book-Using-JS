1e9295e14fb62b1083277441e7ad555cd81038e4
sejal924 <sejalchouhan24@gmail.com>
2025-03-06 21:45:44 +0530

    [Sejal]Add.Added validation for contact details

--- src/index.js
+++ src/index.js
@@ -3,16 +3,35 @@ const Contact = require("./models/Contact");
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-  "Riya",
-  "Verma",
-  "Bhopal",
-  "India",
-  "Madhya Pradesh",
-  "3245",
-  "1234567899",
-  "helloworld@gmail.com"
-);
+try {
+  const contact1 = new Contact(
+    "Sejal ",
+    "Chouhan",
+    "Bhopal",
+    "India",
+    "In",
+    "3245",
+    "1234567890",
+    "helloworld123@gmail.com"
+  );
+  console.log(addressBook.addContact(contact1));
+  console.log(addressBook.getAllContacts());
+} catch (error) {
+  console.error("Error:", error.message);
+}
 
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+  const invalidContact = new Contact(
+    "Bulbul",
+    "Rai",
+    "abctt",
+    "In",
+    "I",
+    "100A01",
+    "12345",
+    "invalid-email"
+  );
+  console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+  console.error("Error:", error.message);
+}
